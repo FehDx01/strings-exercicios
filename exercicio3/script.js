c49d34d7ee22a3d951365679f2a2964c427bb37c
FehDx01 <lessa.fernando01@gmail.com>
2023-11-17 19:12:47 -0300

    exercicio fixacao strings

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,31 @@
-//Crie a const para a frase aqui
+//"Jorge tem uma casa verde e com portão azul, com os dizeres: "`BOAS VINDAS
+
+const frase = '"Jorge tem uma casa verde e com portão azul, com os dizeres: "`BOAS VINDAS, mas não deixe o gato sair`"'
+
+function receberFrase(){
+
+    const fraseAlterada = frase.replace("verde", "rosa")
+    const fraseAlterada1 = fraseAlterada.replace("azul", "laranja")
+    
+   /*  const verificaFrase = fraseAlterada1.includes("verde" && "laranja") */
+    /* console.log(verificaFrase) */
+    
+    console.log(fraseAlterada1)
+    console.log(fraseAlterada1.includes("verde"))
+    console.log(fraseAlterada1.includes("laranja"))    
+    
+    /* console.log(fraseAlterada1.includes("laranja")) */
+    
+    //Extra
+
+    let letrao = "mas não deixe o gato sair"
+    let maiusculaFim = letrao.toUpperCase("mas não deixe o gato sair")
+
+    console.log(`"Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS ${maiusculaFim}`)
+}
+
+receberFrase()
+
+
+
+
